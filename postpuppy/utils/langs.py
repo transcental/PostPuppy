d86928649c1d7f52faae186c156432174e2e2dd7
Alanik <152527270+AlanikREDAWN@users.noreply.github.com>
2025-02-05 13:40:06 -0500

    Update langs.py

--- postpuppy/utils/langs.py
+++ postpuppy/utils/langs.py
@@ -138,5 +138,142 @@ LANGUAGES = {
             "error_heading": ":neodog_notice: Post Puppy",
             "error_description": "arf, arf!\noh no! it looks like i can't fetch your shipments right now :c\nplease check your settings and make sure your URL is correct!",
         },
+    },
+    "dog": {
+        "display_name": "Post Puppy",
+        "icon_emoji": ":neodog_box:",
+        "utils.checker": {
+            "cant_send": {
+                "text": "mrow! :3\nlooks like i can't send to channel <#{}>, please make sure i'm in the channel...meoooow",
+                "blocks": [
+                    {
+                        "type": "section",
+                        "text": {
+                            "type": "mrkdwn",
+                            "text": "mrow! :3\nlooks like i can't send to channel <#{}>, please make sure i'm in the channel...meoooow",
+                        },
+                    },
+                    {
+                        "type": "context",
+                        "elements": [
+                            {
+                                "type": "mrkdwn",
+                                "text": "_mrow mrow mrrrowww (sad meowing noises)_",
+                            }
+                        ],
+                    },
+                ],
+            }
+        },
+        "utils.shipments": {
+            "deleted_shipment": ':neocat_sob: meow mrow~ order of *"{}"* has been cancelled :c i'm sure that\'s okay...hooman! _mrow, mrow!_',
+            "new_shipment": ':neocat_box: mrow meow~ your package, *"{}"*, is on its way, meoww! i found these fancy terms, not sure what it means though, mrow! _({}!)_\noh, and this is what\'s inside!\n{}',
+            "new_shipment_with_tracking": {
+                "pub_msg": f':neocat_laptop_notice: "mrow! your *"{{}}"*, is on its way! a`nd guess what, hooman! you can track it too! visit my <slack://app?team=T0266FRGM&id={env.slack_app_id}|bed> to see where it\'s at, meow mrow!! 🐾\npsst, it has this stuff inside!\n{{}}',
+                "msg": ':neocat_laptop_notice: mrow! mrow! your *"{}"* is on its way! i think there\'s a tracking fish on my pillow\n<{}|throw fish>\na little mouse told me it has these things inside\n{}',
+            },
+            "updated_shipment_with_tracking": {
+                "pub_msg": f':neocat_laptop_notice: mrow, mrow, mrooow!! your *"{{}}"* can be tracked, meow, meow!\ni found a tracking fish for you! it\'s in my  <slack://app?team=T0266FRGM&id={env.slack_app_id}|bed> :3',
+                "msg": ':neocat_laptop_notice: _mrow, meow!!_ i found a <{}|tracking fish> for your *"{}"* on my pillow! \n_mrrow, mrow_',
+            },
+            "type_text_updated": ':neocat_notice: hey hey hey hey hey!!!! hooman, it looks like your *"{}"* is now {}! :3',
+            "description_updated": ':neocat_notice: woah, hooman! your *"{}"* has been updated to {}!!!!\nthat\'s amazing :D',
+            "unknown_update": ':neocat_notice: sowwy, idk what happened, but your *"{}"* has been updated!!! _(excited meowing)_',
+        },
+        "utils.slack": {
+            "setup": "Meow, meow, meow!!!! mrrooow!\n_hai! i'm watching your shipments for you now :3_",
+            "setup_failed": {
+                "text": "meeooow :3\nlooks like i can't send to channel <#{}>, please make sure i'm in the channel uwu",
+                "blocks": [
+                    {
+                        "type": "section",
+                        "text": {
+                            "type": "mrkdwn",
+                            "text": "meeooow :3\nlooks like i can't send to <#{}>, please make sure you added me to the channel uwu :3",
+                        },
+                    },
+                    {
+                        "type": "context",
+                        "elements": [
+                            {
+                                "type": "mrkdwn",
+                                "text": "_meow, meeeeow (sad meowing noises)_",
+                            }
+                        ],
+                    },
+                ],
+            },
+            "disabled": {
+                "text": ":neocat_sob: i'm not watching your shipments anymore :c\n_mrow, meeooowww (sad meowing noises)_",
+                "blocks": [
+                    {
+                        "type": "section",
+                        "text": {
+                            "type": "mrkdwn",
+                            "text": ":neocat_sob: _meow! meow!_ i'm not watching your shipments anymore :c",
+                        },
+                    },
+                    {
+                        "type": "context",
+                        "elements": [
+                            {
+                                "type": "mrkdwn",
+                                "text": "_mrow, meeooooww (sad meowing noises)_",
+                            }
+                        ],
+                    },
+                ],
+            },
+            "verification_sent": {
+                "text": "i've sent you an email with a link to verify your email address, hooman! pwease check your inbox and click the link :3",
+                "blocks": [
+                    {
+                        "type": "section",
+                        "text": {
+                            "type": "mrkdwn",
+                            "text": "i've sent you an email with a link to verify your email address, hooman! pwease check your inbox and click the link :3",
+                        },
+                    },
+                    {
+                        "type": "context",
+                        "elements": [
+                            {
+                                "type": "mrkdwn",
+                                "text": "licks paw excitedly at all those ships",
+                            }
+                        ],
+                    },
+                ],
+            },
+        },
+        "utils.starlette": {
+            "verified_slack": {
+                "text": "Your email has been verified! You can now view your shipments",
+                "blocks": [
+                    {
+                        "type": "section",
+                        "text": {
+                            "type": "mrkdwn",
+                            "text": "Your email has been verified! You can now view your shipments",
+                        },
+                    },
+                    {
+                        "type": "context",
+                        "elements": [
+                            {
+                                "type": "mrkdwn",
+                                "text": "licks paw excitedly at all those ships",
+                            }
+                        ],
+                    },
+                ],
+            }
+        },
+        "views.home": {
+            "heading": ":neodog_box: Post Puppy",
+            "description": "hai! i'm your friendly puppy who loves post nya~! i can help you keep track of your shipments from Hack Club :3\n\nyou haven't set up your viewer URL yet, please do so in the settings!",
+            "error_heading": ":neodog_notice: Post Puppy",
+            "error_description": "arf, arf!\noh no! it looks like i can't fetch your shipments right now :c\nplease check your settings and make sure your URL is correct!",
+        },
     }
 }
