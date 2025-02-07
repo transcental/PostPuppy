6377a34a04c6db87d4d851d98fbeb8535250c23b
Amber <amber@transcental.dev>
2025-02-07 15:48:19 +0000

    shipments not checker

--- postpuppy/utils/shipments.py
+++ postpuppy/utils/shipments.py
@@ -56,7 +56,7 @@ def find_diff(old: list[dict], new: list[dict], lang: dict[str, list[dict] | str
     new_shipments = {shipment.get("title"): shipment for shipment in new}
 
     all_ids = set(old_shipments.keys()).union(new_shipments.keys())
-    lang = lang.get("utils.checker", LANGUAGES.get("dog")["utils.checker"])
+    lang = lang.get("utils.shipments", LANGUAGES.get("dog")["utils.shipments"])
 
     for shipment_id in all_ids:
         msg = ""
